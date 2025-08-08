57f7920ce8e02fd5d1d6616187289dd8d162807d
sanushka64 <sanushkadias@gmail.com>
2025-08-08 16:32:23 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({  
-  plugins: [    
-    tailwindcss(),  
+export default defineConfig({
+  plugins: [
+    react(),       // ✅ React support
+    tailwindcss()  // ✅ Tailwind support
   ],
-   base: '/Gift-A-Future-Sri-Lanka/'
+  base: '/Gift-A-Future-Sri-Lanka/' // ✅ ensures images load on GitHub Pages
 })
